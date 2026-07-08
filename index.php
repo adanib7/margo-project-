fdee8a594c1012c88427c2d9b5152343a283dd1d
adanib7 <adanbertoletti7@gmail.com>
2026-07-08 11:34:18 -0300

    prueba_2

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if (isset($_GET['error'])) {
         <input type="checkbox" id="remember" />
         <label for="remember">Recordarme </label>
       </div>
-      <span class="span">No recuerdas tu contraseña?</span>
+      <span class="span">¿Olvidaste tu contraseña?</span>
     </div>
 
     <button type="submit" class="button-submit">Entrar</button>
